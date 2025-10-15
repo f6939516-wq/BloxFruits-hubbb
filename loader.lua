fbf8c77464ccb4066c76835420039ae043f9567e
f6939516-wq <f6939516@gmail.com>
2025-10-14 19:12:58 -0500

    loader.lua

---
+++ loader.lua
@@ -0,0 +1,2 @@
+-- Blox Fruits Admin Hub Loadstring v2.1 - Compatible con Delta
+loadstring(game:HttpGet("https://raw.githubusercontent.com/f6939516-wq/BloxFruits-hubbb/main/bloxfruits_admin_hub.lua"))()
